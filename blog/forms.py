4ec46abdf511af904f1e7edaf9c0c7786f05a67e
황대겸 <ghkdeorua3461@gmail.com>
2026-05-04 00:01:14 +0900

    update : 서버 배포 최신화

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,9 @@
 from django import forms #ModelForm을 사용하기 위해 import 
 
-from .models import Post
+from .models import Post #view
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('title', 'text',) 
