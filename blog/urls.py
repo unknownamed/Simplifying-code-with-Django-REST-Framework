f8f75570631edf07160ba031b8f198a992cba1cd
황대겸 <ghkdeorua3461@gmail.com>
2026-05-03 19:11:50 +0900

    update : 5/3일까지 실습한 내용

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import re_path # url 함수 쓰려고 -> url함수 사라짐 -> re_path함수 사용
 from . import views # 같은 경로의 views.py에 선언되어있는 post_list사용하려고
+from django.urls import path #path 함수 쓰려고
+
 
 urlpatterns = [ #url패턴 -> 정규식으로 추가
     re_path(r'^$', views.post_list, name='post_list'), #post_list라는 view에서 실제 url처리
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), #정수로구성된 pk값이 오면 -> view에 선언된 post_detail함수 호출
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Post #다른 models.py에 선언된 Post 클래스 가져오기(동일한 디렉토리는 .py안 붙여도됨)
 from django.utils import timezone #now() 함수를 통해 현재시간보다 이전에 게시된 글들을 필터링하기위해
 
 def post_list(request):
-        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        posts = Post.objects.all().order_by('-published_date')
         #{'posts' : post} -> key-value 형식으로 'posts'는 템플릿 내에서 쓸 이름이고 posts는 위의 SQL로 조회된 내용이다.
         return render(request, 'blog/post_list.html',  {'posts': posts}) #blog/post_list.html을 렌더함수가 브라우저에 반환해줌
+
+def post_detail(request, pk): 
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
