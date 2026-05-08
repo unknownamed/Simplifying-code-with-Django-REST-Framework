340dd23f1fdb5f4397be8d66e9ad26fbb859a924
황대겸 <ghkdeorua3461@gmail.com>
2026-05-09 02:19:48 +0900

    update : 5/9일까지 진행분

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from django.urls import re_path, include #blog 앱에서 url들고오도록 추가
+from django.urls import re_path, include  # blog 앱에서 url들고오도록 추가
 from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, viewsets  # 직렬화 코드 파일 따로 추가
+from .serializer import UserSerializer  # 직렬화 코드 가져오기
+
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -32,14 +35,17 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls), #관리자 페이지 URL
+    path("admin/", admin.site.urls),  # 관리자 페이지 URL
     path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    re_path(r'', include('blog.urls')), #blog에서 가져오는 것 추가, r은 정규식 문자때문에 쓰는게 관례(\d, \w), (정규 표현식 r ''이라서 모든 주소 포함?)포괄적인 기본 주소는 맨밑으로가야됨
+    re_path(
+        r"", include("blog.urls")
+    ),  # blog에서 가져오는 것 추가, r은 정규식 문자때문에 쓰는게 관례(\d, \w), (정규 표현식 r ''이라서 모든 주소 포함?)포괄적인 기본 주소는 맨밑으로가야됨
 ]
